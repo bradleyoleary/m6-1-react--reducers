59cf69e8b2cf6770635af7f19596ab0c962b01af
Bradley O'Leary <bradley_oleary@outlook.com>
2020-11-16 13:52:18 -0500

    Ex 2 - Managing state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/App';
+import App from "./components/App";
+import { SeatContext } from "./components/SeatContext";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <SeatContext>
+    <App />
+  </SeatContext>,
+  rootElement
+);
